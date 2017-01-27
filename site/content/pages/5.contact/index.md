cf0d59e2152efa2e7a28a63470c69d6377e0386e
Merlin Mason <info@merlinmason.co.uk>
2017-01-27 12:52:03 +0000

    Add contact details to contact page

--- site/content/pages/5.contact/index.md
+++ site/content/pages/5.contact/index.md
@@ -1,6 +1,9 @@
----
+masthead:
+  - /assets/backgrounds/features-1485428288.jpg
+form_heading: Getting in touch with the team is easy
+contact_heading: Other ways to contact us
+heading: Getting in touch with the team is easy
 title: Contact
 template: contact
+fieldset: contact
 id: de627bca-7595-429e-9b41-ad58703916d7
----
-Contact me! 
--- site/themes/precursive/js-src/main.js
+++ site/themes/precursive/js-src/main.js
@@ -1,3 +1,4 @@
+import $ from "jquery";
 import Home from "./pages/home";
 import CssAnimation from "./modules/css-animation";
 import CaseStudies from "./modules/case-studies";
@@ -5,3 +6,8 @@ import CaseStudies from "./modules/case-studies";
 Home();
 CssAnimation();
 CaseStudies();
+
+$(".js-show-intercom").on("click", (e) => {
+    e.preventDefault();
+    window.Intercom("show");
+});
